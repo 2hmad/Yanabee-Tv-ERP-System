3bcaa07be0a8e921b00f1ed668457a42d0edd95d
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2021-04-11 20:11:43 +0200

    Update yanabee.sql

--- yanabee.sql
+++ yanabee.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 11, 2021 at 01:15 AM
+-- Generation Time: Apr 11, 2021 at 08:10 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.15
 
