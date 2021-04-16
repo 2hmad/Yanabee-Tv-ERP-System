f9589545b763bc43489c4dcb6e28fe4002362dee
coduxcompany <eng.ahmedmohamed.2002@gmail.com>
2021-04-17 00:25:17 +0200

    .

--- application/views/backend/admin/accountant/create.php
+++ application/views/backend/admin/accountant/create.php
@@ -30,7 +30,7 @@
         <option value=""><?php echo get_phrase('select_a_gender'); ?></option>
         <option value="Male"><?php echo get_phrase('male'); ?></option>
         <option value="Female"><?php echo get_phrase('female'); ?></option>
-        <option value="Others"><?php echo get_phrase('others'); ?></option>
+        
       </select>
       <small id="" class="form-text text-muted"><?php echo get_phrase('provide_gender'); ?></small>
     </div>
--- application/views/backend/student/student/single_student_admission.php
+++ application/views/backend/student/student/single_student_admission.php
@@ -60,7 +60,7 @@
                     <option value=""><?php echo get_phrase('select_gender'); ?></option>
                     <option value="Male"><?php echo get_phrase('male'); ?></option>
                     <option value="Female"><?php echo get_phrase('female'); ?></option>
-                    <option value="Others"><?php echo get_phrase('others'); ?></option>
+                    
                 </select>
             </div>
         </div>
