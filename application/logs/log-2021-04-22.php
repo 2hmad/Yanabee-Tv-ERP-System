13e1faae31a0fbff4177dcc1c5a1bc1b8afc11be
coduxcompany <eng.ahmedmohamed.2002@gmail.com>
2021-04-22 00:33:28 +0200

    update

---
+++ application/logs/log-2021-04-22.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-04-22 00:00:05 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'yanabee'@'localhost' (using password: YES) C:\xampp\htdocs\yanabee\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-22 00:00:05 --> Unable to connect to the database
+ERROR - 2021-04-22 00:00:05 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'yanabee'@'localhost' (using password: YES) C:\xampp\htdocs\yanabee\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-22 00:00:05 --> Severity: Warning --> ini_set(): A session is active. You cannot change the session module's ini settings at this time C:\xampp\htdocs\yanabee\system\libraries\Session\Session_driver.php 205
+ERROR - 2021-04-22 00:00:05 --> Severity: Warning --> session_start(): Failed to initialize storage module: user (path: C:\xampp\tmp) C:\xampp\htdocs\yanabee\system\libraries\Session\Session.php 143
+ERROR - 2021-04-22 00:00:05 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'yanabee'@'localhost' (using password: YES) C:\xampp\htdocs\yanabee\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-22 00:00:05 --> Unable to connect to the database
+ERROR - 2021-04-22 00:00:05 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'yanabee'@'localhost' (using password: YES) C:\xampp\htdocs\yanabee\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-04-22 00:00:05 --> Unable to connect to the database
+ERROR - 2021-04-22 00:00:05 --> Query error: Access denied for user 'yanabee'@'localhost' (using password: YES) - Invalid query: SELECT *
+FROM `settings`
+WHERE `id` = 1
+ERROR - 2021-04-22 00:00:05 --> Severity: error --> Exception: Call to a member function row_array() on bool C:\xampp\htdocs\yanabee\application\helpers\common_helper.php 15
